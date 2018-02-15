b50da70f112558687dd28469f1136d1e7b8d3a1f
Raoul Lefmann <raoul.lefmann@gmail.com>
2018-02-15 16:50:43 +0100

    Implemented marginalInference! method. Fixed bug in marginalInference! with complete evidence. Added exclamation mark to inference method names.

--- src/inference.jl
+++ src/inference.jl
@@ -6,25 +6,26 @@
 Marginal inference with incomplete evidence. Queries of type P(E=e),
 where E ⊆ X.
 """
-function marginalInference(spn::SumProductNetwork, x::AbstractVector, e::BitVector)
-
+function marginalInference!(spn::SumProductNetwork, x::AbstractVector, e::BitVector)
+	setInput!(spn, x, e)
+	return eval!(spn)
 end
 
 
 """
 Marginal inference with complete evidence. Queries of type P(X=x).
 """
-function marginalInference(spn::SumProductNetwork, x::AbstractVector)
-	n, d = size(x)
+function marginalInference!(spn::SumProductNetwork, x::AbstractVector)
+	d = length(x)
 	e::BitVector = trues(d)
-	return marginalInference(spn, x, e)
+	return marginalInference!(spn, x, e)
 end
 
 
 """
 Conditional inference. Queries of type P(Q=q | E=e).
 """
-function conditionalInference(spn::SumProductNetwork, x::AbstractVector, q::BitVector, e::BitVector)
+function conditionalInference!(spn::SumProductNetwork, x::AbstractVector, q::BitVector, e::BitVector)
 
 end
 
@@ -33,6 +34,6 @@ end
 Given values for some set of variables E ⊆ X, find the most
 likely assignment to all remaining variables Q = X\E.
 """
-function mpeInference(spn::SumProductNetwork, x::AbstractVector, e::BitVector)
+function mpeInference!(spn::SumProductNetwork, x::AbstractVector, e::BitVector)
 
 end
