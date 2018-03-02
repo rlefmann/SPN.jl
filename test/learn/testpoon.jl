e6486c9ebaac20e2a76c3850653d7540cfbf0ad4
Raoul Lefmann <raoul.lefmann@gmail.com>
2018-03-02 21:29:28 +0100

    Rewritten testpoon.jl.

--- test/learn/testpoon.jl
+++ test/learn/testpoon.jl
@@ -1,3 +1,32 @@
+"""
+A (m x n) rectangle has c = m*(m+1)*n*(n+1)/4 subrectangles.
+The Poon architecture has `nsum` sum nodes for every subrectangle.
+Only the full rectangle (base region) is represented by just one sum node.
+"""
+function numSumNodesBaseres1(w, h, nsum)
+	numSubRects = w*(w+1)*h*(h+1)/4
+	return (numSubRects-1)*nsum + 1
+end
+
+
+"""
+A ixj rectangle can be decomposed in i+j-2 ways.
+There are therefore
+
+\sum_{i=1}^w \sum_{j=1}^h (w+1-i)*(h+1-j)*(i+j-2)
+
+decompositions of subrectangles.
+Each decomposition is represented by nsum^2 product nodes.
+"""
+function numProdNodesBaseres1(w, h, nsum)
+	numDecompositions = 0
+	for i in 1:w, j in 1:h
+		numDecompositions += (w+1-i)*(h+1-j)*(i+j-2)
+	end
+	return numDecompositions * nsum^2
+end
+
+
 width = 2
 height = 3
 baseres = 1
@@ -6,21 +35,8 @@ nsum = 5
 s = structureLearnPoon(width, height, baseres, nsum)
 spn = SumProductNetwork(s)
 
-#=
-A (m x n) rectangle has m*(m+1)*n*(n+1)/4 subrectangles.
-Therefore the 2x3 rectangle in this case has 2*3*3*4/4 = 18 subrectangles.
-Each subrectangle other than the full rectangle is represented by 5 sum nodes.
-The full rectangle (root) is represented by a single sum node.
-Each decomposition is represented by 5^2 = 25 product nodes.
-Therefore the total number of nodes has to be 17*(25+5)+1*(25+1) = 536.
-
-TODO: the explanation isn't quite correct, because in general
-#rectangles != #decompositons.
-=#
-
-@test length(spn) == 536
-
-
+expectedNumNodes = numSumNodesBaseres1(width, height, nsum) + numProdNodesBaseres1(width, height, nsum)
+@test length(spn) == expectedNumNodes
 
 width = 2
 height = 2
@@ -30,4 +46,5 @@ nsum = 5
 s = structureLearnPoon(width, height, baseres, nsum)
 spn = SumProductNetwork(s)
 
-@test length(spn) == 191
+expectedNumNodes = numSumNodesBaseres1(width, height, nsum) + numProdNodesBaseres1(width, height, nsum)
+@test length(spn) == expectedNumNodes
