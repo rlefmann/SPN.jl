9f76c6e477842135c61d75a034ceb21fd9819b1d
Raoul Lefmann <raoul.lefmann@gmail.com>
2018-04-25 16:50:35 +0200

    Matrix version of parameterLearnEM

--- src/learn/parameter.jl
+++ src/learn/parameter.jl
@@ -112,7 +112,7 @@ function parameterLearnEM!(spn::SumProductNetwork, x::AbstractMatrix; iterations
 			for node in spn.order
 				if typeof(node) == SumNode
 					for cidx in 1:length(node.children)
-						r = exp(node.children[cidx].logval) * node.weights[cidx]# / exp(node.logval) * exp(node.logdrv)
+						r = exp(node.children[cidx].logval) * node.weights[cidx] / exp(node.logval) * exp(node.logdrv)  # TODO: are the last two terms important?
 						node.counts[cidx] += r
 					end
 				end
@@ -133,6 +133,67 @@ function parameterLearnEM!(spn::SumProductNetwork, x::AbstractMatrix; iterations
 
 	return llhvals
 end
+
+
+function parameterLearnEM1!(spn::SumProductNetwork, x::AbstractMatrix; iterations=30)
+	n, d = size(x)
+	m = numNodes(spn)
+
+	# normalize all sum nodes:
+	normalize!(spn)
+	setIDs!(spn)
+
+	# the loglikelihood of the data under the current model:
+	model_llhvals = Vector{Float64}(iterations)
+
+
+	llhvals = Matrix{Float64}(m, n)
+	logdrvs = Matrix{Float64}(m, n)
+	counts = Dict{Int, Vector{Float64}}()
+	
+	for node in spn.order
+		if isa(node, SumNode)
+			counts[node.id] = zeros(length(node.children))
+		end
+	end
+
+	
+	for t in 1:iterations
+
+		# set counts of all sum nodes to 0.0:
+		#resetCounts!(spn)
+
+		#indices = 1:n
+
+		eval!(spn, x, llhvals)
+		computeDerivatives!(spn, x, llhvals, logdrvs)
+		for node in spn.order
+			if isa(node, SumNode)
+				childids = [child.id for child in node.children]
+				r = exp.(llhvals[childids,:]) .* node.weights ./ exp.(llhvals[node.id,:])' .* exp.(logdrvs[node.id,:])'
+				counts[node.id] = vec(sum(r,2))
+			end
+		end
+
+		# set weights according to counts:
+		for node in spn.order
+			if isa(node, SumNode)
+				#node.weights = node.counts
+				node.weights = counts[node.id]
+                normalize!(node)
+                # reset counts:
+                counts[node.id] = zeros(length(node.children))
+			end
+		end
+
+		model_llhvals[t] = dataLikelihood!(spn, x)
+		@printf "iteration %d:\t llh=%d\n" t model_llhvals[t]
+	end
+
+	return model_llhvals
+end
+
+
 
 
 function parameterLearnGD!(spn::SumProductNetwork, x::AbstractMatrix; iterations=30, learnrate=0.1)
